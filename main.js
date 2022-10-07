04ac21adec30e61bffa4659c4cc56f81a98e2a71
Morganero <morganero1@gmail.com>
2022-10-07 10:05:05 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+[
+    {
+        id: 1,
+        topics: [ "strategy", "professional", "help" ],
+        text: "Review all my old code before asking for help or looking at hints.",
+        author: "Samantha Maas",
+        date: "2020-11-09"
+    },
+    {
+        id: 2,
+        topics: [ "thinking", "help" ],
+        text: "I have found that slowing down and thinking about the problem, and writing out the comments makes it vastly easier to write code.",
+        author: "Gib Jeffries",
+        date: "2021-01-18"
+    },
+    {
+        id: 3,
+        topics: [ "reminder", "github" ],
+        text: "Need to have all my code backed up to Github by the end of the week.",
+        author: "Rebecca Parker",
+        date: "2021-02-25"
+    },
+    {
+        id: 4,
+        topics: [ "strategy", "github" ],
+        text: "Always work on a branch and submit a PR, even if I'm working on my own project.",
+        author: "Red Tilson",
+        date: "2020-11-25"
+    }
+]
+const filteredNotes = []
+const criteria =github
+//check to see if array includes whatever criteria you put as a value in criteria variable using .includes method
+
+for (const note of notes) (
+    if ()
+)
